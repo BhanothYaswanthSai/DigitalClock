175ac5605ad7b58e74ba73b18f49712030f67913
BhanothYaswanthSai <bhanothyaswanth2801@gmail.com>
2024-09-06 10:45:21 +0530

    First commit on DigitalClock

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css'
+import DigitalClock from './DigitalClock.jsx';
+
+
+function App() {
+  return (
+    <>
+    <DigitalClock/>
+   
+    </>
+  );
+}
+
+export default App
